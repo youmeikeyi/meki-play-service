2cc5d0a2e7940014e3c1860505a2f8825c300115
jinchao.xu <jinchao.xu3@renren-inc.com>
2014-11-04 17:16:39 +0800

    first commit

---
+++ src/main/java/com/meki/play/api/service/impl/TestServiceImpl.java
@@ -0,0 +1,9 @@
+package com.meki.play.api.service.impl;
+
+/**
+ * User: jinchao.xu
+ * Date: 14-9-19
+ * Time: 上午10:56
+ */
+public class TestServiceImpl {
+}
---
+++ src/main/java/com/meki/play/api/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.meki.play.api.user.service.impl;
+
+/**
+ * User: jinchao.xu
+ * Date: 14-9-19
+ * Time: 上午10:58
+ */
+public class UserServiceImpl {
+}
